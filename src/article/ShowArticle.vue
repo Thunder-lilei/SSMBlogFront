4c9ca7785e2e1aa6b6e4ef6b6ed9c907d8c2cab7
Thunder-lilei <lilei.kuge@qq.com>
2021-01-05 18:38:28 +0800

    在查看页面添加修改功能

--- src/article/ShowArticle.vue
+++ src/article/ShowArticle.vue
@@ -1,5 +1,10 @@
 <template>
-  <div v-html="blog" class="markdown-body">
+  <div>
+    <div v-html="blog" class="markdown-body">
+
+    </div>
+    <br/>
+    <el-button @click="toUpdateArticle(articleId)" type="primary" icon="el-icon-edit" circle></el-button>
   </div>
 </template>
 
@@ -13,6 +18,7 @@ export default {
   data() {
     return {
       blog: '',
+      articleId: '',
     }
   },
   mounted () {
@@ -24,6 +30,34 @@ export default {
       this.$axios.post('/article/getArticle').then(response => {
         if (response.data.message === 'success') {
           this.blog = marked(response.data.article.articleContent)
+          this.articleId = response.data.article.articleId
+        } else {
+          that.$message({
+            showClose: true,
+            message: response.data.message,
+            type: 'warning'
+          });
+        }
+      }).catch(
+        function (error) {
+          that.$message({
+            showClose: true,
+            message: error,
+            type: 'warning'
+          });
+        })
+    },
+    toUpdateArticle:function (articleId) {
+      this.setArticle(articleId)
+      this.$router.push('/Article');
+    },
+    setArticle:function (articleId) {
+      const that = this
+      let data = new URLSearchParams();
+      data.append("articleId", articleId)
+      this.$axios.post('/article/setArticle', data).then(response => {
+        if (response.data.message === 'success') {
+
         } else {
           that.$message({
             showClose: true,
